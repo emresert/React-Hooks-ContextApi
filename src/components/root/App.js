b57638a7f38dfcdde62920247cf7facd9557ad8a
Emre Sert <09370754@ksnet.local>
2020-03-13 15:23:11 +0300

    Context and navbar with toggle is completed

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from '../navbar/Navbar';
+import BookList from '../booklist/BookList';
+import ThemeToggle from '../toogle/ThemeToggle';
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar></Navbar>
+     <BookList></BookList>
+     <ThemeToggle></ThemeToggle>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './components/root/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import {ThemeContextProvider} from './components/contexts/ThemeContext'
+
+ReactDOM.render(
+     
+    <ThemeContextProvider>
+        <App />
+    </ThemeContextProvider>
+
+, document.getElementById('root'));
+
+
 serviceWorker.unregister();
